8694904323c2cf4311974b8361e770d3f076934e
ZiadurRehman718 <ziadhah718@gmail.com>
2024-02-27 18:54:19 +0500

    Todo created

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [text , setText] = useState ("")
+  const [todo , setTodo] = useState ([])
+  let [newtodo , setNewtodo] = useState ([])
+  
+  function addTodo(event) {
+    event.preventDefault();
+    console.log(text);
+    setTodo([...todo, text]);
+    console.log(todo);
+    setText("")
+  }
+
+  function deletetodo(index) {
+    console.log(index);
+    todo.splice(index, 1);
+    setTodo([...todo]);
+  }
+
+  function edittodo(index) {
+    console.log(index);
+    todo.splice(index, 1);
+    newtodo = prompt ("enter new task")
+    setTodo([...todo, newtodo]);
+    setNewtodo([...newtodo])
+  }
+  return (
+    <>
+    <h1>Note Todo's</h1>
+    <form onSubmit={addTodo}>
+    <input type="text" placeholder='Enter your task' onChange={(e) => setText(e.target.value)} value={text}/>
+    <button type='submit'>Add Task</button>
+    </form>
+    <ul>
+      {todo.map((item , index) => {
+        return <li key={index}>{item}
+        <button onClick={()=>deletetodo(index)}>Delete</button>
+        <button onClick={()=>edittodo(index)}>Edit</button>
+        </li>
+      })}
+    </ul>
+    </>
+  )
+}
+
+export default App
